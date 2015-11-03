1f82ee8036ff7b31e21123806be866bfa83fcf32
Steven Silvester <steven.silvester@ieee.org>
2015-11-02 20:06:29 -0600

    Rebuild libs

--- lib/menus/index.d.ts
+++ lib/menus/index.d.ts
@@ -1,10 +1,6 @@
 import { ICommandMenuItem } from './menuiteminterface';
 import { IExtension } from 'phosphor-plugins';
 import { IDisposable } from 'phosphor-disposable';
-export * from './menuiteminterface';
-export * from './menumanagerinterface';
-export * from './menusolver';
-export * from './menusolverfunctions';
 /**
  * The interface required for `menu:items` extension point.
  */
--- lib/menus/menusolver.d.ts
+++ lib/menus/menusolver.d.ts
@@ -1,18 +1,15 @@
 import { ICommandMenuItem } from './menuiteminterface';
-import { MenuBar } from 'phosphor-menus';
+import { MenuItem } from 'phosphor-menus';
 /**
- * A class to solve the relationships between menu items and allow custom
- * menu creation.
+ * Solve the relationships between menu items and allow custom menu creation.
+ *
+ * #### Notes
+ * We use topsort (topological sorting) to find the order of menu items
+ * based on their names and constraints.
+ * The constrains form dependencies (Before(y) means directed edge x->y)
+ * and therefore we can use topsort to find a suitable order. We won't
+ * use a full DAG topsort; we only solve one level of the menu at a
+ * time because the menu is just a simple tree, for which we need the
+ * results one branch at a time.
  */
-export declare class MenuSolver {
-    /**
-     * We use topsort (topological sorting) to find the order of menu items
-     * based on their names and constraints.
-     * The constrains form dependencies (Before(y) means directed edge x->y)
-     * and therefore we can use topsort to find a suitable order. We won't
-     * use a full DAG topsort; we only solve one level of the menu at a
-     * time because the menu is just a simple tree, for which we need the
-     * results one branch at a time.
-     */
-    static solve(items: ICommandMenuItem[]): MenuBar;
-}
+export declare function solveMenu(items: ICommandMenuItem[]): MenuItem[];
