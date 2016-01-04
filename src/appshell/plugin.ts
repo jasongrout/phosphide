c4b035341030b98d3d81d9b4ff1d1b95f4176315
S. Chris Colbert <sccolbert@gmail.com>
2016-01-04 13:18:39 -0500

    update to work with widget rc releases

--- src/appshell/plugin.ts
+++ src/appshell/plugin.ts
@@ -23,6 +23,10 @@ import {
 } from 'phosphor-dockpanel';
 
 import {
+  Panel
+} from 'phosphor-panel';
+
+import {
   IChangedArgs
 } from 'phosphor-properties';
 
@@ -35,7 +39,7 @@ import {
 } from 'phosphor-stackedpanel';
 
 import {
-  Panel, Title, Widget
+  Title, Widget
 } from 'phosphor-widget';
 
 import {
@@ -120,6 +124,8 @@ class AppShell extends Widget implements IAppShell {
     leftHandler.stackedPanel.id = 'p-left-stack';
     rightHandler.stackedPanel.id = 'p-right-stack';
     dockPanel.id = 'p-main-dock-panel';
+
+    dockPanel.spacing = 8; // TODO make this configurable?
 
     hsplitPanel.orientation = SplitPanel.Horizontal;
     hsplitPanel.spacing = 1; // TODO make this configurable?
@@ -290,14 +296,17 @@ class SideBarHandler {
    */
   private _refreshVisibility(): void {
     this._sideBar.setHidden(this._sideBar.titleCount() === 0);
-    this._stackedPanel.setHidden(this._stackedPanel.currentWidget === null);
+    this._stackedPanel.setHidden(this._sideBar.currentTitle === null);
   }
 
   /**
    * Handle the `currentChanged` signal from the sidebar.
    */
   private _onCurrentChanged(sender: SideBar, args: IChangedArgs<Title>): void {
-    this._stackedPanel.currentWidget = this._findTitleWidget(args.newValue);
+    let oldWidget = this._findTitleWidget(args.oldValue);
+    let newWidget = this._findTitleWidget(args.newValue);
+    if (oldWidget) oldWidget.hide();
+    if (newWidget) newWidget.show();
     this._refreshVisibility();
   }
 
