b8d5d6fab57b860ef9a73f93b7e843fcdd684281
S. Chris Colbert <sccolbert@gmail.com>
2016-02-17 18:35:29 -0500

    fill in basic palette registry functionality

--- src/services/paletteregistry.ts
+++ src/services/paletteregistry.ts
@@ -8,6 +8,18 @@
 'use strict';
 
 import {
+  AbstractPaletteModel, StandardPaletteModel
+} from 'phosphor-commandpalette';
+
+import {
+  DisposableDelegate, IDisposable
+} from 'phosphor-disposable';
+
+import {
+  ISignal, Signal
+} from 'phosphor-signaling';
+
+import {
   ABCCommandRegistry
 } from './commandregistry';
 
@@ -20,8 +32,68 @@ import {
  *
  */
 export
-abstract class ABCPaletteRegistry {
+interface IPaletteItem {
+  /**
+   *
+   */
+  command: string;
 
+  /**
+   *
+   */
+  text: string;
+
+  /**
+   *
+   */
+  icon?: string;
+
+  /**
+   *
+   */
+  caption?: string;
+
+  /**
+   *
+   */
+  category?: string;
+
+  /**
+   *
+   */
+  className?: string;
+}
+
+
+/**
+ *
+ */
+export
+abstract class ABCPaletteRegistry {
+  /**
+   * A signal emitted when a command is triggered by the palette.
+   */
+  get commandTriggered(): ISignal<ABCPaletteRegistry, string> {
+    return Private.commandTriggeredSignal.bind(this);
+  }
+
+  /**
+   *
+   */
+  get model(): AbstractPaletteModel {
+    return this.getModel();
+  }
+
+  /**
+   * Add command palette items to the palette registry.
+   *
+   * @param items - The array of items to add to the registry.
+   *
+   * @returns A disposable which will remove the added items.
+   */
+  abstract add(items: IPaletteItem[]): IDisposable;
+
+  protected abstract getModel(): AbstractPaletteModel;
 }
 
 
@@ -39,8 +111,65 @@ class PaletteRegistry extends ABCPaletteRegistry {
     this._shortcuts = shortcuts;
   }
 
+  /**
+   * Add command palette items to the palette registry.
+   *
+   * @param items - The array of items to add to the registry.
+   *
+   * @returns A disposable which will remove the added items.
+   */
+  add(items: IPaletteItem[]): IDisposable {
+
+    let optionsArray = items.map(item => {
+
+      // let commandExists = this._commandRegistry.has(item.id);
+      // if (!commandExists) return null;
+
+      let seq = this._shortcuts.sequenceFor(item.command);
+      let shortcut = seq ? Private.formatSequence(seq) : '';
+
+      let options = {
+        handler: this._executeCommand,
+        args: item.command,
+        text: item.text,
+        shortcut: shortcut,
+        icon: item.icon || '',
+        caption: item.caption || '',
+        category: item.category || ''
+      };
+
+      return options;
+    });
+
+    if (optionsArray.length === 0) {
+      return new DisposableDelegate(null);
+    }
+
+    let paletteItems = this._model.addItems(optionsArray);
+
+    return new DisposableDelegate(() => {
+      this._model.removeItems(paletteItems);
+    });
+  }
+
+  /**
+   *
+   */
+  protected getModel(): AbstractPaletteModel {
+    return this._model;
+  }
+
+  /**
+   * The private command handler function.
+   */
+  private _executeCommand = (id: string) => {
+    this.commandTriggered.emit(id);
+    this._commands.execute(id);
+  };
+
   private _commands: ABCCommandRegistry;
   private _shortcuts: ABCShortcutRegistry;
+  private _model = new StandardPaletteModel();
 }
 
 
@@ -62,5 +191,17 @@ const paletteRegistryProvider = {
  *
  */
 namespace Private {
+  /**
+   * A signal emitted when a command is triggered by the palette.
+   */
+  export
+  const commandTriggeredSignal = new Signal<ABCPaletteRegistry, string>();
 
+  /**
+   *
+   */
+  export
+  function formatSequence(seq: string[]): string {
+    return seq.map(s => s.trim().replace(/\s+/g, '-')).join(' ');
+  }
 }
